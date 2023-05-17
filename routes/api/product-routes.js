1c9d44b2d3a6fe5b87fcc92ddec427da90d3f78b
zeuz <zeuz@zeuzs-iMac.local>
2023-05-17 13:47:00 -0400

    set up productRoutes to use try catch and be async

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,22 +1,34 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
+// find all products
+router.get("/", async (req, res) => {
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product;
+    res.status(200).json(productData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
+// find a single product by its `id`
+router.get("/:id", async (req, res) => {
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product;
+    res.status(200).json(productData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +60,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,8 +101,15 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+// delete one product by its `id` value
+router.delete("/:id", async (req, res) => {
+  try {
+    const productData = await Product;
+    res.status(200).json(productData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
